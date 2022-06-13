4d5bd490ea4a2865b3b6e3a8f4210c7512e550f0
YuJiaHao <goot121433@gmail.com>
2022-06-13 21:38:54 +0800

    Add contain in Create page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React,{useState} from 'react'
+import { Container,Form } from 'react-bootstrap'
 
 const Create = () => {
-  return (
-    <Container>
-        <h1 className='mt-5'>Create</h1>
-        <p>This is practice note-app</p>
-    </Container>
-  )
+
+    const [form ,setForm] = useState({title:'',text:''}) ;
+
+    function handleChanged(event) {
+        const {value, name} = event.target;
+        setForm({...form, [name]:value});
+    }
+
+    return (
+        <Container>
+            <Form className='mt-3' style={{width:"50%"}}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Title </Form.Label>
+                    <Form.Control onChange={handleChanged} value = {form.title} name='title' type="text" placeholder="Enter title" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Text</Form.Label>
+                    <Form.Control onChange={handleChanged} value = {form.text} name='text' type="text" placeholder="Enter text" />
+                </Form.Group>
+                
+            </Form>
+        </Container>
+
+    )
 }
 
 export default Create
